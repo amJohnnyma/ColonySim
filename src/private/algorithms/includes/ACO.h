954be57b40982a60238148cfc8ed56bc920a0967
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-05-09 17:16:46 +0200

    also works ? Note to self -> may not work next time :)

--- src/private/algorithms/includes/ACO.h
+++ src/private/algorithms/includes/ACO.h
@@ -23,8 +23,8 @@ class ACO
         std::vector<Cell*> adjCells;
         int worldWidth;
         int worldHeight;
-        double pF = 0; //pheremone influnce
-        double hF = 10; // heuristic influence
+        double pF = 0.3; //pheremone influnce
+        double hF = 0.8; // heuristic influence
         double pheremoneEvap = 0.1;
         std::unordered_set<Cell*> visited;
         int numberAnts = 1;
