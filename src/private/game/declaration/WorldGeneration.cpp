52b30dd9b31c01f059563575760a70add4463a5f
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-05-21 20:06:42 +0200

    big refactor

--- src/private/game/declaration/WorldGeneration.cpp
+++ src/private/game/declaration/WorldGeneration.cpp
@@ -1,4 +1,5 @@
 #include "../headers/WorldGeneration.h"
+#include "WorldGeneration.h"
 
 WorldGeneration::WorldGeneration(unsigned int seed, int xWidth, int yWidth, int cellSize)
 {
@@ -25,114 +26,133 @@ std::vector<std::unique_ptr<Cell>> WorldGeneration::getResult() {
     return std::move(grid); // Transfer ownership safely
 }
 
-void WorldGeneration::generateTerrain()
+std::array<pheromone,2> WorldGeneration::createPheromones(int x, int y)
 {
-    for(int x = 0; x < width; x++)
+    pheromone p1;
+    p1.x = x;
+    p1.y = y;
+    p1.type = "findFood";
+    p1.strength = 0.1;
+
+    pheromone p2;
+    p2.x = x;
+    p2.y = y;
+    p2.type = "returnHome";
+    p2.strength = 0.1;
+
+    return {p1, p2};
+}
+
+double WorldGeneration::generateDifficulty()
+{
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_real_distribution<> dis(0.001, 1.0);
+    return dis(gen);
+}
+
+std::unique_ptr<Circle> WorldGeneration::createCellShape(int x, int y, float size, double difficulty)
+{
+    auto shape = std::make_unique<Circle>(x, y, size, 8);
+    // Set the fill color with difficulty affecting alpha channel
+    shape->setFillColor(sf::Color(0, 255, 0, static_cast<sf::Uint8>(difficulty * 255)));
+    return shape;
+}
+
+std::unique_ptr<Cell> WorldGeneration::createCell(int x, int y, float cellSize)
+{
+    auto cell = std::make_unique<Cell>();
+
+    CellData cd;
+    cd.type = "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+    cd.difficulty = generateDifficulty();
+
+    auto pheromones = createPheromones(x, y);
+    cd.p[0] = pheromones[0];
+    cd.p[1] = pheromones[1];
+
+    cell->cellShape = createCellShape(x, y, cellSize / 4, cd.difficulty);
+    cell->x = x;
+    cell->y = y;
+    cell->data = std::move(cd);
+
+    return cell;
+}
+
+
+std::unique_ptr<sf::RectangleShape> WorldGeneration::createShape(sf::Color fillColor, int x, int y, float cellSize)
+{
+    auto shape = std::make_unique<sf::RectangleShape>();
+    shape->setSize(sf::Vector2f(cellSize, cellSize));
+    shape->setPosition(x * cellSize, y * cellSize);
+    shape->setOutlineThickness(1.f);
+    shape->setOutlineColor(sf::Color::Red);
+    shape->setFillColor(fillColor);
+    return shape;
+}
+
+std::unique_ptr<Ant> WorldGeneration::createAnt(int x, int y)
+{
+    auto shape = createShape(sf::Color::White, x, y, cellSize);
+    Cell* cell = grid[x * width + y].get();
+    return std::make_unique<Ant>(y, x, "ant", 10, std::move(shape), cell);
+}
+
+std::unique_ptr<Location> WorldGeneration::createBase(int x, int y)
+{
+    auto shape = createShape(sf::Color::Yellow, x, y, cellSize);
+    Cell* cell = grid[x * width + y].get();
+    return std::make_unique<Location>(x, y, "Base", 100000, std::move(shape), cell);
+}
+
+void WorldGeneration::logAllEntities()
+{
+    for (auto& cellPtr : grid)
     {
-        for(int y = 0; y < height; y++)
+        for (auto& entityPtr : cellPtr->data.entities)
         {
-          //  std::unique_ptr<Shape> shape = std::make_unique<Circle>(x, y, cellSize/4, 4);
-          std::unique_ptr<Circle> shape = std::make_unique<Circle>(x,y,cellSize/4, 8);
-
-            std::unique_ptr<Cell> newC = std::make_unique<Cell>();     
-            CellData cd;
-            pheromone p,p2;
-            p.x = x;
-            p.y = y;
-            p.type = "findFood";
-
-            p2.x = x;
-            p2.y = y;
-            p.type = "returnHome";
-
-
-            cd.type = "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
-            //random between 0-1. 0 easy, 1 hard
-            std::random_device rd;
-            std::mt19937 gen(rd());
-            std::uniform_real_distribution<> dis(0.001,1.0);
-            double randomVal = dis(gen);
-            cd.difficulty = randomVal;
-
-            p.strength = 0.1; //arbitrary init
-            p2.strength = 0.1;
-
-
-            shape->setFillColor(sf::Color(0,255,0, (randomVal*255)));    
-
-            cd.p[0] = p;
-            cd.p[1] = p2;
-            newC->cellShape = std::move(shape);   
-            newC->x = x;
-            newC->y = y;
-            newC->data = std::move(cd);
-            grid.push_back(std::move(newC));
+            std::cout << entityPtr->getName() << ", "
+                      << entityPtr->getX() << ", "
+                      << entityPtr->getY() << std::endl;
         }
     }
-
-
 }
+
+
+
+void WorldGeneration::generateTerrain()
+{
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            grid.push_back(createCell(x, y, cellSize));
+        }
+    }
+}
+
 
 void WorldGeneration::generateEntities(int num, int col)
 {
-    std::vector<std::pair<int,int>> vis;
-
     int xVal = conf::worldSize.x / 2;
     int yVal = conf::worldSize.y / 2;
-
-    // Cache width and height if you use them
-    int w = width;
-    int h = height;
 
     for (int i = 0; i < col; i++)
     {
         for (int k = 0; k < num; k++)
         {
-            // Create and configure Ant's rectangle shape
-            auto antShape = std::make_unique<sf::RectangleShape>();
-            antShape->setSize(sf::Vector2f(cellSize, cellSize));
-            antShape->setPosition(xVal * cellSize, yVal * cellSize);
-            antShape->setOutlineThickness(1.f);
-            antShape->setOutlineColor(sf::Color::Red);
-            antShape->setFillColor(sf::Color::White);
-
-            // Get cell pointer
-            Cell* cell = grid[xVal * w + yVal].get();
-
-            // Create Ant entity with unique_ptr to shape
-            auto antEntity = std::make_unique<Ant>(yVal, xVal, "ant", 10, std::move(antShape), cell);
-
-            vis.push_back({ yVal, xVal });
-
-            // Add entity to cell
-            cell->data.entities.push_back(std::move(antEntity));
+            auto ant = createAnt(xVal, yVal);
+            grid[xVal * width + yVal]->data.entities.push_back(std::move(ant));
         }
 
-        // Create and configure Base's rectangle shape
-        auto baseShape = std::make_unique<sf::RectangleShape>();
-        baseShape->setSize(sf::Vector2f(cellSize, cellSize));
-        baseShape->setPosition(xVal * cellSize, yVal * cellSize);
-        baseShape->setOutlineThickness(1.f);
-        baseShape->setOutlineColor(sf::Color::Red);
-        baseShape->setFillColor(sf::Color::Yellow);
+        auto base = createBase(xVal, yVal);
+        grid[xVal * width + yVal]->data.entities.push_back(std::move(base));
 
-        Cell* cell = grid[xVal * w + yVal].get();
-
-        auto baseEntity = std::make_unique<Location>(xVal, yVal, "Base", 100000, std::move(baseShape), cell);
-        cell->data.entities.push_back(std::move(baseEntity));
-
-        // Debug print all entities in the grid
-        for (auto& cellPtr : grid)
-        {
-            for (auto& entityPtr : cellPtr->data.entities)
-            {
-                std::cout << entityPtr->getName() << ", " 
-                          << entityPtr->getX() << ", " 
-                          << entityPtr->getY() << std::endl;
-            }
-        }
+        // Logging moved to separate function
+        logAllEntities();
     }
 }
+
 
 void WorldGeneration::assignTextures()
 {
