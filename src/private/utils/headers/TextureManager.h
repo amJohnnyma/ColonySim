9739a73aa4e73b818fe7a2c0a9285308da5c09f6
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-05-27 19:32:19 +0200

    texture manager

--- src/private/utils/headers/TextureManager.h
+++ src/private/utils/headers/TextureManager.h
@@ -1,11 +1,9 @@
 #ifndef TEXTUREMANAGER_H
 #define TEXTUREMANAGER_H
 
-#include <SFML/Graphics.hpp>
 #include <unordered_map>
-#include <string>
+#include <SFML/Graphics.hpp>
 #include <memory>
-#include <iostream>
 
 class TextureManager {
 public:
