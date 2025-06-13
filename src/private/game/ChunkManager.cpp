73946e84733607f1471109604cce4be8f45bb691
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-06-13 12:25:18 +0200

    Some ants get stuck on a location

--- src/private/game/ChunkManager.cpp
+++ src/private/game/ChunkManager.cpp
@@ -66,7 +66,7 @@ void ChunkManager::ensureChunksAround(int centerChunkX, int centerChunkY, int ra
                 continue;
 
               //  std::cout << "Mkae new chunk" << std::endl;
-                if (grid.size() < 256) // hard limit
+                if (grid.size() < conf::max_world_size.x * conf::max_world_size.y) // hard limit
                 {
                    // std::cout << "Loaded grid " << grid.size() << std::endl;
                      //std::cout << "World size: " <<  conf::worldSize.x << ", " << conf::worldSize.y << std::endl;
